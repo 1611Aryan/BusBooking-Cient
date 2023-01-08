40167076b32451c4e74c3063a2ae5f15873602ef
Aryan <aryan2563737@gmail.com>
2023-01-08 10:44:11 +0530

    Base URL Updated

--- src/API/baseurl.js
+++ src/API/baseurl.js
@@ -1,4 +1,6 @@
 const baseURL =
-  process.env.NODE_ENV !== "production" ? "http://localhost:5000" : ""
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5000"
+    : "https://busbooking.herokuapp.com"
 
 export default baseURL
